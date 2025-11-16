8f5552f83297b1985bd82c9d9f27ae88cef70e56
jGuru <java@guru.com>
2025-11-16 09:15:22 +0100

    Added for debugging

--- LibreRestart.ps1
+++ LibreRestart.ps1
@@ -1,8 +1,8 @@
 #restart Docker
 docker-compose -f docker-compose.windows.yml restart
 
-# Wait for containers to start (6 seconds)
-Start-Sleep -Seconds 6
+# Wait for containers to start (4 seconds)
+Start-Sleep -Seconds 4
 
 # Script to open the default browser with a specific URL
 $url = "http://localhost:3080/"
